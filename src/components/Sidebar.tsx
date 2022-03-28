7e03a073c3c14d6186ba22139a820930b0a8faaf
任意のユーザ名 <exsrx97@gmail.com>
2022-03-29 00:03:22 +0900

    サイドバーレイアウト修正

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 export const Sidebar = () => {
   return (
-    <div className="fixed main-heading px-10">
+    <div className="fixed main-heading px-10 mt-10">
      <ul>
         <li>Top stories</li>
         <li>Business</li>
