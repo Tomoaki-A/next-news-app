c67f928679deef83afc87fe39f6d3fa8c4ce2117
Tomoaki Asano <tasano@vitalify.jp>
2022-03-31 10:55:49 +0900

    テスト

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <div className="main-bg w-screen px-10 py-4 fixed shadow-xl">
       <Link href="/">
         <a>
-          <h1 className="main-heading">Web News</h1>
+          <h1 className="main-heading">WebNews</h1>
         </a>
       </Link>
     </div>
